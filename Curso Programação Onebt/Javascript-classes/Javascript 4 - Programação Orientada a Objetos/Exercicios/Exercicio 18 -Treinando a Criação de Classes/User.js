11f14990ce812ffe264bc291d3084b062232b393
Renan Amorim <renanamorim89@hotmail.com>
2023-02-11 09:07:44 -0300

    Treinando a Criação de Classes

---
+++ Curso Programação Onebt/Javascript-classes/Javascript 4 - Programação Orientada a Objetos/Exercicios/Exercicio 18 -Treinando a Criação de Classes/User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(fullname, email, password) {
+    this.fullname = fullname;
+    this.email = email;
+    this.password = password;
+  }
+  login(email, password) {
+    if (this.email === email && this.password === password) {
+      console.log("Login Realizado com Sucesso!!");
+    } else {
+      console.log("Falha ao realizar o Login - Email ou Senha incorreto");
+    }
+  }
+}
+
+const john = new User("John Doe", "john@email.com", "1234");
+
+console.log(john);
+john.login("john@email.com", "1234");
